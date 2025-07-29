2d6aa50e9c3bf5d57b1c5ca5b3f34de58a1a216a
ayush gupta <ag298744@gmail.com>
2025-07-29 21:11:10 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -9,15 +9,17 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/enquiry", routerPath)
 
+const ConnectDb = async () => {
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log("DATABASE IS CONECTED");
+        app.listen(process.env.PORT || 3000, () => {
+            console.log("server is running");
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("DATABASE IS CONECTED");
-    app.listen(process.env.PORT || 3000, () => {
-        console.log("server is running");
+        })
+
+    }).catch((error) => {
+        console.error(error);
 
     })
-
-}).catch((error) => {
-    console.error(error);
-
-})
+}
+ConnectDb()
